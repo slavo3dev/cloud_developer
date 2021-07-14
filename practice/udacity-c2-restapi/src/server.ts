a6f200031e255baf1fd2ff91bb20e2d6b5b4d598
slavo3dev <slavo@slavo3.com>
2021-07-14 17:04:43 -0400

    testing

--- practice/udacity-c2-restapi/src/server.ts
+++ practice/udacity-c2-restapi/src/server.ts
@@ -12,12 +12,12 @@ import { V0MODELS } from './controllers/v0/model.index';
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.PORT || 8080; // default port to listen
+  const port = process.env.PORT || 8000; // default port to listen
   
   app.use(bodyParser.json());
 
   //CORS Should be restricted
-  app.use(function(req, res, next) {
+  app.use((req, res, next) =>  {
     res.header("Access-Control-Allow-Origin", "http://localhost:8100");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();

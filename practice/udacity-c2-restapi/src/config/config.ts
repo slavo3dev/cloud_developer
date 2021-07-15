223e5053a878429fa4c2158fe4ffb1824ae06819
slavo3dev <slavo@slavo3.com>
2021-07-15 14:06:38 -0400

    adding secretes

--- practice/udacity-c2-restapi/src/config/config.ts
+++ practice/udacity-c2-restapi/src/config/config.ts
@@ -1,11 +1,11 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1a",
     "aws_profile": "default",
     "aws_media_bucket": "udagram-ruttner-dev"
   },
